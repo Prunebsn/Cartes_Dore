5951c703e364b382f78f3dfa34f28cc3a7cca8ef
Prunebsn <p.besson@eodd.fr>
2024-05-23 16:44:29 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// Initialiser la carte
+var map = L.map('map').setView([46.2276, 2.2137], 6); // Centré sur la France
+
+// Ajouter une couche de base
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
+}).addTo(map);
+
+// Charger les données GeoJSON
+fetch('geojson/bassins_versants.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data).addTo(map);
+    });
+
+fetch('geojson/reseau_hydrographique.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data).addTo(map);
+    });
+
+fetch('geojson/points_interet.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            onEachFeature: function (feature, layer) {
+                if (feature.properties && feature.properties.link) {
+                    layer.bindPopup('<a href="' + feature.properties.link + '" target="_blank">Voir le PDF</a>');
+                }
+            }
+        }).addTo(map);
+    });
