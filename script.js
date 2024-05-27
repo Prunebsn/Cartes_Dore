d7d7cf003a68857a47c81dac765ea60b61314ab0
Prunebsn <p.besson@eodd.fr>
2024-05-27 16:13:46 +0200

    Update script.js
    Signed-off-by: Prunebsn <p.besson@eodd.fr>

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var map = L.map('map').setView([45.7721, 2.9611], 8);
 
 // Charger une couche de base OpenStreetMap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
