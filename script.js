f2e752ff62e5dc54d45dab287ef5bc7b94934631
Prunebsn <p.besson@eodd.fr>
2024-05-27 15:09:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,26 @@
-// Initialiser la carte avec la projection Lambert 93
-var crs = new L.Proj.CRS('EPSG:2154',
-    '+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
-    {
-        resolutions: [8192, 4096, 2048, 1024, 512, 256, 128],
-        origin: [0, 0],
-        bounds: L.bounds([0, 0], [700000, 6600000])
-    }
-);
+// Initialiser la carte
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-// Créer la carte en utilisant la projection Lambert 93
-var map = L.map('map', {
-    crs: crs,
-    center: [6600000, 700000], // Coordonnées approximatives du centre de la France en Lambert 93
-    zoom: 6
-});
-
-// Ajouter une couche de base OpenStreetMap
+// Charger une couche de base OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Charger et afficher les données géospatiales
+// Charger la couche BV_Stations avec la projection Lambert 93
 fetch('geojson/BV_Stations.geojson')
     .then(response => response.json())
     .then(data => {
         L.geoJSON(data, {
             style: function (feature) {
                 return {
-                    className: 'BV_Stations' // Ajouter la classe CSS aux stations
+                    className: 'bv-station' // Ajouter la classe CSS aux stations
                 };
             }
         }).addTo(map);
     });
 
-// Charger et afficher la couche réseau hydrographique
+// Charger la couche réseau hydrographique avec la projection Lambert 93
 fetch('geojson/reseau_hydrographique.geojson')
     .then(response => response.json())
     .then(data => {
@@ -47,7 +33,7 @@ fetch('geojson/reseau_hydrographique.geojson')
         }).addTo(map);
     });
 
-// Charger et afficher la couche des stations avec des popups
+// Charger la couche des stations avec la projection Lambert 93 et popups
 fetch('geojson/Stations.geojson')
     .then(response => response.json())
     .then(data => {
@@ -64,7 +50,8 @@ fetch('geojson/Stations.geojson')
             },
             onEachFeature: function (feature, layer) {
                 if (feature.properties && feature.properties.link) {
-                    var pdfUrl = '/pdfs/' + feature.properties.link; // Corriger le chemin vers le dossier PDF
+                    // Concaténer le chemin du dossier PDF avec la valeur de la propriété 'link'
+                    var pdfUrl = '/pdf/' + feature.properties.link;
                     layer.bindPopup('<a href="' + pdfUrl + '" target="_blank">Voir le PDF</a>');
                 }
             }
