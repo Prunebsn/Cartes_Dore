4ed4fe340d0e0b31b7d9e81e6dc81aa8858f8ad8
Prunebsn <p.besson@eodd.fr>
2024-05-27 12:46:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,32 @@
-// Définir la projection Lambert 93
-var crs = new L.Proj.CRS('EPSG:2154',
-    '+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
-    {
-        resolutions: [8192, 4096, 2048, 1024, 512, 256, 128],
-        origin: [0, 0],
-        bounds: L.bounds([0, 0], [700000, 6600000])
-    }
-);
+// Initialiser la carte avec Leaflet sans spécifier de projection
+var map = L.map('map');
 
-// Initialiser la carte avec la projection Lambert 93
-var map = L.map('map', {
-    crs: crs
-});
-
-// Charger une couche de base OpenStreetMap
+// Ajouter une couche de base OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19
 }).addTo(map);
 
-// Définir les coordonnées du centre de la France en Lambert 93
-var franceCenter = crs.project(L.latLng(46.603354, 1.888334)); // Coordonnées du centre de la France en Lambert 93
+// Centrer la carte sur un emplacement spécifique
+map.setView([51.505, -0.09], 13);
 
-// Centrer la carte sur la France en Lambert 93
-map.setView([franceCenter.y, franceCenter.x], 6);
-
-// Charger la couche BV_Stations avec la projection Lambert 93
+// Charger et afficher les données géospatiales
 fetch('geojson/BV_Stations.geojson')
     .then(response => response.json())
     .then(data => {
         L.geoJSON(data, {
             style: function (feature) {
                 return {
-                    className: 'bv-station' // Ajouter la classe CSS aux stations
+                    className: 'BV_Stations' // Ajouter la classe CSS aux stations
                 };
             }
         }).addTo(map);
     });
 
-// Charger la couche réseau hydrographique avec la projection Lambert 93
+// Charger et afficher la couche réseau hydrographique
 fetch('geojson/reseau_hydrographique.geojson')
     .then(response => response.json())
     .then(data => {
         L.geoJSON(data, {
-            crs: crs, // Spécifier la projection Lambert 93
             style: function (feature) {
                 return {
                     className: 'reseau-hydrographique' // Ajouter la classe CSS au réseau hydrographique
@@ -51,7 +35,7 @@ fetch('geojson/reseau_hydrographique.geojson')
         }).addTo(map);
     });
 
-// Charger la couche des stations avec la projection Lambert 93 et popups
+// Charger et afficher la couche des stations avec des popups
 fetch('geojson/Stations.geojson')
     .then(response => response.json())
     .then(data => {
@@ -68,12 +52,9 @@ fetch('geojson/Stations.geojson')
             },
             onEachFeature: function (feature, layer) {
                 if (feature.properties && feature.properties.link) {
-                    // Concaténer le chemin du dossier PDF avec la valeur de la propriété 'link'
                     var pdfUrl = '/pdf/' + feature.properties.link;
                     layer.bindPopup('<a href="' + pdfUrl + '" target="_blank">Voir le PDF</a>');
                 }
-            },
-            crs: crs // Spécifier la projection Lambert 93
+            }
         }).addTo(map);
     });
-
